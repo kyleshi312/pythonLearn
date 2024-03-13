f4a2ed316d18ea5f2c7d7248ba1322aec76d9a27
kyleshi <shihuan_shi@163.com>
2024-03-13 23:49:51 +0800

    type of variant

---
+++ 01_Python基础/debug.md
@@ -0,0 +1,3 @@
+![alt text](image-15.png)
+
+debug 时显示 变量类型
---
+++ 01_Python基础/hm_04_buy apple.py
@@ -0,0 +1,11 @@
+# price
+price = 8.5
+# select
+weight = 7.5
+# amount
+money = price * weight
+
+# 4. 只要买苹果 就反悔5块钱
+money = money - 5
+
+print(money)
